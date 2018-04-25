776d4364b1f29e5496f4514268a3e3bc20f9e167
justin <justin.han15@outlook.com>
2018-04-25 10:35:17 +0100

    fixed code. now correct

--- app/src/main/java/com/example/a2hanj43/sensors/MainActivity.java
+++ app/src/main/java/com/example/a2hanj43/sensors/MainActivity.java
@@ -7,12 +7,14 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
 
     Sensor accel, magField;
 
@@ -40,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         magFieldValues = new float[3];
         orientationMatrix = new float[16];// combination of rotation and translation need 4 by 4 matrix
         orientations = new float[3];
+
     }
 
 
@@ -48,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         TextView xacc = (TextView)findViewById(R.id.xacc);
         TextView yacc = (TextView)findViewById(R.id.yacc);
         TextView zacc = (TextView)findViewById(R.id.zacc);
+        TextView match = (TextView)findViewById(R.id.match);
 
         //values is an array of the acceleration values in the axes
 //        xacc.setText(df.format(ev.values[0]));
@@ -56,23 +60,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if(ev.sensor == accel)
         {
             for (int i = 0; i<3; i++){
-                accelValues[i] = ev.values [0];//ev.values.clone();//.clone() makes a copy of array but is memory inefficient
+                accelValues[i] = ev.values [i];//ev.values.clone();//.clone() makes a copy of array but is memory inefficient
             }
 
         }
         else if (ev.sensor == magField)
         {
             for (int i = 0; i<3; i++){
-                magFieldValues[i] = ev.values [0];//ev.values.clone();//.clone() makes a copy of array but is memory inefficient
+                magFieldValues[i] = ev.values [i];//ev.values.clone();//.clone() makes a copy of array but is memory inefficient
             }
+
+
+
+        }
+
+        if (ev.values[0] > 100 && ev.values[0] < 150) {
+            Log.d("sensors", "there is a match");
+            System.out.println("match");
+            match.setText("match");
+
         }
 
         SensorManager.getRotationMatrix(orientationMatrix, null, accelValues, magFieldValues);
         SensorManager.getOrientation(orientationMatrix, orientations);
 
-        xacc.setText(df.format(ev.values[0] * 180/Math.PI));
-        yacc.setText(df.format(ev.values[1] * 180/Math.PI));
-        zacc.setText(df.format(ev.values[2] * 180/Math.PI));
+        xacc.setText(df.format(orientations[0] * 180/Math.PI));
+        yacc.setText(df.format(orientations[1] * 180/Math.PI));
+        zacc.setText(df.format(orientations[2] * 180/Math.PI));
+
+
 
     }
 
